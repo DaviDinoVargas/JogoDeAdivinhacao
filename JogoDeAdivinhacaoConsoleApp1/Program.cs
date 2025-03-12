c02c2e5f7837c13988143edbc295fbf52fb037f1
davidinovargas <davidinovargas@gmail.com>
2025-03-12 14:03:19 -0300

    Atualização nas exibições de telas do Jogo de Adivinhação

--- JogoDeAdivinhacaoConsoleApp1/Program.cs
+++ JogoDeAdivinhacaoConsoleApp1/Program.cs
@@ -12,28 +12,44 @@
                 Console.WriteLine("         Jogo de Adivinhação");
                 Console.WriteLine("----------------------------------------");
                 Console.WriteLine("----------------------------------------");
-                Console.WriteLine("        Escola a Dificuldade: ");
+                Console.WriteLine("        Escolha a Dificuldade: ");
                 Console.WriteLine("       1- Fácil     10 tentativas");
                 Console.WriteLine("       2- Normal    5 tentativas");
                 Console.WriteLine("       3- Dificil   3 tentativas");
                 Console.WriteLine("----------------------------------------");
+
                 int totalTentativas = 0;
+                bool entradaValida = false;
 
-                Console.Write("Digite sua dificuldade");
-                string opcaoDificuldade = Console.ReadLine();
+                while (!entradaValida)
+                {
+                    Console.Write("Digite sua dificuldade: ");
+                    string opcao = Console.ReadLine().Trim();
 
-                if (opcaoDificuldade == "1")
-                    totalTentativas = 10;
-                else if (opcaoDificuldade == "2")
-                    totalTentativas = 5;
-                else
-                    totalTentativas = 3;
+                    if (opcao == "1")
+                    {
+                        totalTentativas = 10;
+                        entradaValida = true;
+                    }
+                    else if (opcao == "2")
+                    {
+                        totalTentativas = 5;
+                        entradaValida = true;
+                    }
+                    else if (opcao == "3")
+                    {
+                        totalTentativas = 3;
+                        entradaValida = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Opção inválida! Escolha 1, 2 ou 3.");
+                    }
+                }
 
-                    // gerar um número secreto aleatório
-
-                    Random geradorDeNumeros = new Random();
+                // gerar um número secreto aleatório
+                Random geradorDeNumeros = new Random();
                 int numeroSecreto = geradorDeNumeros.Next(1,21);
-
 
                 for (int tentativas = 1; tentativas <= totalTentativas; tentativas++)
                 {
@@ -50,9 +66,7 @@
                         Console.WriteLine("----------------------------------------");
                         Console.WriteLine("Você acertou o número secreto!");
                         Console.WriteLine("----------------------------------------");
-
                         break;
-
                     }
 
                     else if (numeroDigitado > numeroSecreto)
@@ -76,13 +90,11 @@
                         break;
                     }
 
-                    Console.WriteLine("Aperte Enter");
+                    Console.WriteLine("Aperte <Enter>");
                     Console.ReadLine();
                 }
-
                 Console.Write("Deseja continuar? (s/N): ");
                 string opcaoContinuar = Console.ReadLine().ToUpper();
-
                 if (opcaoContinuar != "S")
                     break;
 
