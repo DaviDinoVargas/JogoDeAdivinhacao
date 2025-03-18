0f3fcf18ced3579f0ff0edee4fa8168123a48794
Davi <139391138+DaviDinoVargas@users.noreply.github.com>
2025-03-18 11:04:36 -0300

    Create  JogoDeAdivinhacao.cs

---
+++ JogoDeAdivinhacaoConsoleApp1/JogoDeAdivinhacao.cs
@@ -0,0 +1,36 @@
+namespace JogoDeAdivinhacaoConsoleApp1
+{
+    public static class JogoDeAdivinhacao
+    {
+        public static int numeroSecreto;
+        public static int totalTentativas;
+
+        public enum ResultadoTentativa
+        {
+            Acertou,
+            NumeroMenor,
+            NumeroMaior
+        }
+
+        public static void IniciarNovoJogo(int tentativas)
+        {
+            Random geradorDeNumeros = new Random();
+            numeroSecreto = geradorDeNumeros.Next(1, 21);
+            totalTentativas = tentativas;
+        }
+        public static ResultadoTentativa VerificarTentativa(int numeroDigitado)
+        {
+            if (numeroDigitado == numeroSecreto)
+                return ResultadoTentativa.Acertou;
+
+            return numeroDigitado > numeroSecreto ?
+            ResultadoTentativa.NumeroMaior :
+            ResultadoTentativa.NumeroMenor;
+        }
+
+        public static int ObterNumeroSecreto() //numero secreto
+        {
+            return numeroSecreto;
+        }
+    }
+}
